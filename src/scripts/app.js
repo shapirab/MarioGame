0a8075b878e6725d3327c00a13589104c4b472a6
shapirab <shapirab@jamd.ac.il>
2023-12-10 16:12:37 +0200

    10/12/23 added background and hero images to the project

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,18 @@
 import Game from './game.js';
+
+const canvas = document.getElementById('main-canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ctx = canvas.getContext('2d');
+
+let game = new Game(canvas.width, canvas.height);
+
+function animate(){
+    requestAnimationFrame(animate);
+    game.update();
+    game.draw(ctx);
+
+}
+
+animate();
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,7 +1,12 @@
 export default class Game{
-    constructor(){}
+    constructor(canvasWidth, canvasHeight){
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+    }
 
     update(){}
 
-    draw(ctx){}
+    draw(ctx){
+        ctx.drawImage(marioImg, 0, 0, 30, 40, 100, 100, 120, 160);
+    }
 }
