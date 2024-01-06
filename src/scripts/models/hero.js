843f53c46fafc5a34c28a2cc08409abc75e0f13a
shapirab <shapirab@jamd.ac.il>
2024-01-06 18:58:09 +0200

    6/1/24 selected another hero, resized and positioned on ground

--- src/scripts/models/hero.js
+++ src/scripts/models/hero.js
@@ -2,37 +2,64 @@ export default class Hero{
     constructor(canvasWidth, canvasHeight){
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
-
-        this.heroWidth = 200;
-        this.heroHeight = 200;
-      
-        this.frameX = 0;
         
+        //this determains the frequency of changing of frames, thus
+        //extablishing the running pace
         this.framesPerSecond = 10;
         this.frameInterval = 1000 / this.framesPerSecond;
         this.frameTimer = 0;
-        this.maxFrame = 8;
+
+        this.imageCounter = 1;
+        this.maxImages = 8;
+        this.imageName = `marioImg_${this.imageCounter}`;
+
+        this.sizeFactor = 2.5;
+        this.width = 641;
+        this.height = 542;
+
+        
+        this.ground = this.canvasHeight - (this.height / this.sizeFactor);
+        this.position = {
+            x: 100,
+            y: this.ground
+        }
+        console.log('ground location: ')
+        console.log(this.ground);
+        console.log(this.onGround());
+        console.log('canvas height: ')
+        console.log(this.canvasHeight);
+
+    }
+
+    onGround(){
+        return this.position.y === this.ground;
     }
 
     update(deltaTime){
-        //sprite animation
-        if(this.frameTimer > this.frameInterval){           
-            if(this.frameX >= this.maxFrame){               
-                this.frameX = 0;
+        if(this.frameTimer > this.frameInterval){
+            if(this.imageCounter >= this.maxImages){
+                this.imageCounter = 1;
             }
             else{
-                this.frameX++;
+                this.imageCounter++;
             }
             this.frameTimer = 0;
         }
-        else{           
-            this.frameTimer += deltaTime;            
+        else{
+            this.frameTimer += deltaTime;
         }
+        
+        this.image = new Image();
+        this.image.src = `../images/hero/Adventure Girl/png/Run (${this.imageCounter}).png`;
+        this.image.width = this.width / this.sizeFactor;
+        this.image.height = this.height / this.sizeFactor;
     }
 
     draw(ctx){
-        ctx.drawImage(marioImg, this.frameX * this.heroWidth, 0 * this.heroHeight, 
-            this.heroWidth, this.heroHeight, 
-            100, this.canvasHeight - this.heroHeight, this.heroWidth, this.heroHeight);
+        // ctx.drawImage(marioImg, this.frameX * this.heroWidth, 0 * this.heroHeight, 
+        //     this.heroWidth, this.heroHeight, 
+        //     100, this.canvasHeight - this.heroHeight, 
+        //     this.heroWidth, this.heroHeight);
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.image.width, this.image.height);
     }
 }
