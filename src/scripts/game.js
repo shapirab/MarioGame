9d14c956aaa6ffc17af4a939b29e295e02d8e0c0
shapirab <shapirab@jamd.ac.il>
2024-03-29 18:11:22 +0300

    29/3/24 completed the worm enemy class

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,25 +2,25 @@ import Hero from "./models/hero.js";
 import Ramp from "./models/ramp.js";
 import Input from "./input.js";
 import Layer from "./models/layer.js";
-import { Worm } from "./models/enemies.js";
+import { FlyingEnemy, Worm } from "./models/enemies.js";
 
 export default class Game {
   constructor(canvasWidth, canvasHeight) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
-    
+
     this.hero = new Hero(canvasWidth, canvasHeight);
     this.ramp = new Ramp(canvasWidth, canvasHeight, this.hero);
-    
+
     this.input = new Input();
     this.verticalSpeedConstant = 5;
     this.maxVerticalWidth = 400;
-    
+
     this.layers = [];
     this.createLayers();
 
     this.enemies = [];
-    this.enemyTypes = ['Worm'];
+    this.enemyTypes = ["Worm", "FlyingEnemy"];
     this.enemyInterval = 500;
     this.enemyTimer = 0;
   }
@@ -44,41 +44,44 @@ export default class Game {
     this.layers.push(layer);
   }
 
-  #createEnemies(){
+  #createEnemies() {
     let enemyIndex = Math.floor(Math.random() * this.enemyTypes.length);
     let randomEnemy = this.enemyTypes[enemyIndex];
-    if(randomEnemy == 'Worm'){
+    if (randomEnemy == "Worm") {
       this.enemies.push(new Worm(this));
+    } else if (randomEnemy == "FlyingEnemy") {
+      this.enemies.push(new FlyingEnemy(this));
     }
   }
 
   update(deltaTime) {
     this.hero.update(deltaTime, this.input);
     this.ramp.update(deltaTime);
-    if(this.hero.currentState == this.hero.states[1] ||
-      this.hero.currentState == this.hero.states[2]){
+    if (
+      this.hero.currentState == this.hero.states[1] ||
+      this.hero.currentState == this.hero.states[2]
+    ) {
       this.layers.forEach((layer) => {
-        layer.update();        
+        layer.update();
       });
     }
-    
-    if(this.enemyTimer > this.enemyInterval){
+
+    if (this.enemyTimer > this.enemyInterval) {
       this.#createEnemies();
       this.enemyTimer = 0;
-    }
-    else{
-        this.enemyTimer+= deltaTime;
+    } else {
+      this.enemyTimer += deltaTime;
     }
     this.enemies.forEach((enemy) => {
       enemy.update(deltaTime);
     });
   }
 
-  draw(ctx) {   
+  draw(ctx) {
     this.layers.forEach((layer) => {
       layer.draw(ctx);
     });
-   
+
     this.hero.draw(ctx);
     this.ramp.draw(ctx);
 
--- src/scripts/models/enemies.js
+++ src/scripts/models/enemies.js
@@ -11,66 +11,89 @@ class Enemy {
     this.frameTimer = 0;
   }
 
-  
-
   update(deltatime) {
     this.position.x += this.velocity.x * deltatime;
     this.position.y += this.velocity.y * deltatime;
 
     //animation
-    //this.animateEnemy(deltatime);    
+    this.animateEnemy(deltatime);
   }
 
   draw(ctx) {
     ctx.drawImage(
-        this.image,
-        this.frameX * this.width,
-        this.frameY * this.height,
-        this.width,
-        this.height,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
 
-  animateEnemy(deltatime){
-    if(this.frameTimer > this.frameInterval){
+  animateEnemy(deltatime) {
+    if (this.frameTimer > this.frameInterval) {
       //serve the next frame
-      if(this.frameX < this.maxFrames){
-          this.frameX++;
-      }
-      else{
+      if (this.frameX < this.maxFrames) {
+        this.frameX++;
+      } else {
         this.frameX = 0;
       }
       //reset frameTimer
       this.frameTimer = 0;
-    }
-    else{
+    } else {
       //increase the frameTimer
       this.frameTimer += deltatime;
     }
   }
 }
 
-export class Worm extends Enemy{
-  constructor(game){
+export class Worm extends Enemy {
+  constructor(game) {
     super(game);
+    this.game = game;
     this.image = enemy_worm;
     let spriteWidth = 482;
     let spriteHeight = 60;
     this.width = spriteWidth / 6;
     this.height = 60;
+    this.maxFrames = 5;
 
     this.position = {
-      x: 200,
-      y: 200
-    }
+      x: this.game.canvasWidth,
+      y: this.game.canvasHeight - this.height,
+    };
     this.velocity = {
-      x: 0,
-      y: 0
-    }
+      x: -0.5,
+      y: 0,
+    };
+  }
+}
 
+export class FlyingEnemy extends Enemy {
+  constructor(game){
+    super(game);
+    this.game = game;
+    this.image = enemy_fly;
+    let spriteWidth = 360;
+    let spriteHeight = 44;
+    this.width = spriteWidth / 6;
+    this.height = 44;
+    this.maxFrames = 5;
+
+    this.position = {
+      x: this.game.canvasWidth,
+      y: getRandomHeight(),
+    };
+    this.velocity = {
+      x: -0.5,
+      y: 0,
+    };
+  }
+
+  getRandomHeight(){
+    
   }
 }
